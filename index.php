b5658b89d484ebce9e9d065b83ebe9f85aae100e
Mahbub <mahbubhussaincse@gmail.com>
2024-01-21 12:21:45 +0600

    App class written

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
 <?php
 
 declare(strict_types=1);
+
+use App\Helpers\App;
+use App\Helpers\Config;
+
 require_once __DIR__ . '/vendor/autoload.php';
+
+
+$application = new App();
+echo $application->getServerTime()->format('Y-m-d H:i:s') . PHP_EOL;
+echo $application->isDebugMode() . PHP_EOL;
+echo $application->getEnviornment() . PHP_EOL;
+echo $application->getLogPath() . PHP_EOL;
+
+// $config =  Config::get('app');
+
+// var_dump($config);
+
+if ($application->isRunningFromConsole()) {
+    echo "running from console";
+} else {
+    echo "Running from Web Server";
+}
---
+++ src/Helpers/Config.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Helpers;
+
+
+class Config
+{
+
+    public static function get(string $filename, string $key = null): mixed
+    {
+        $fileContent = self::getFileContent($filename);
+        if ($key == null) {
+            return $fileContent;
+        }
+        return isset($fileContent[$key]) ? $fileContent[$key] : [];
+    }
+
+    public static function getFileContent(string $filename): array
+    {
+        $filecontent = [];
+        try {
+            $path = realpath(sprintf(__DIR__ . '/../Configs/%s.php', $filename));
+            if (file_exists($path)) {
+                $filecontent = require $path;
+            }
+        } catch (\Throwable $th) {
+            // die($th->getMessage());
+            throw new \RuntimeException(
+                sprintf('The Specified file %s was not found', $filename)
+            );
+        }
+        return $filecontent;
+    }
+}
